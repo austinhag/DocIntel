0b6c011f2c86a83bf0d0b50a2409852376ee0f45
Austin Hagerty <ahagerty@microsoft.com>
2024-02-15 10:38:05 -0600

    Added model type

--- BahDocIntel.py
+++ BahDocIntel.py
@@ -1,13 +1,18 @@
 from azure.ai.formrecognizer import DocumentAnalysisClient
 from azure.core.credentials import AzureKeyCredential
-import json, requests
+import json, requests, time
 
 # Import endpoints and api key
 from env import azure_endpoint, api_key
 
+start_time = time.time()
+
 # Path to PDF file
 #pdf_path = "sample2_medical_report.pdf"
 pdf_path = "https://images.drlogy.com/assets/uploads/lab/pdf/HISTOPATHOLOGY-COLONOSCOPY-WITH-POLYPECTOMY-BIOPSY-report-format-example-sample-template-Drlogy-lab-report.pdf"
+
+model_type = "prebuilt-document"
+#model_type = "prebuilt-read"
 
 # Create Doc Intelligence Client
 client = DocumentAnalysisClient(azure_endpoint, AzureKeyCredential(api_key))
@@ -20,7 +25,7 @@ else:
        pdf_bytes = pdf_file.read()
 
 # Analyze the document
-poller = client.begin_analyze_document("prebuilt-document", pdf_bytes)
+poller = client.begin_analyze_document(model_type, pdf_bytes)
 result = poller.result()
 
 # Output the results
@@ -56,4 +61,8 @@ json_result = json.dumps(result_dict, indent=4)
 # Write the JSON result to a file
 with open("output-full.json", "w") as output_file:
     output_file.write(json_result)
-    
+
+# Calculate script run time and print    
+end_time = time.time()
+total_time = end_time - start_time
+print(f"Total script run time: {total_time} seconds")
